4a7f4ea5eeb9707089116e14f75653d99304b657
Ayomide <ayomidesunday31@gmail.com>
2023-08-10 03:19:17 +0100

    Clear dev for proper gitflow

--- src/__mocks__/axios.js
+++
@@ -1,6 +0,0 @@
-export default {
-  get: jest.fn(() => Promise.resolve({})),
-  post: jest.fn(() => Promise.resolve({})),
-  put: jest.fn(() => Promise.resolve({})),
-  delete: jest.fn(() => Promise.resolve({})),
-};
